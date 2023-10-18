19a1fb9eaff0b6665d816ead8e765fda852e6a0c
Jonathan Khanlian <jdkhanlian@gmail.com>
2023-10-18 18:52:47 -0400

    updated code thorugh 1:54:24 in tutorial

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 )
 
+// entry point
 func main() {
 	// var are mutable; const are immutable
 	// types can be inferred
@@ -34,8 +35,8 @@ func main() {
 	// but a "slice", which is defined as a variable length array (like a python list?), is a better solution. no length defined ahead of time.
 	var bookings []string
 
-	// there is no while loop in Go, only a for loop.
-	for {
+	// there is no while loop in Go, only a for loop. so while true is just for. and while condition is for condition.
+	for remainingTickets > 0 {
 		// define a type because we don't assign a value so the program can't infer it yet
 		var firstName string
 		var lastName string
@@ -55,31 +56,62 @@ func main() {
 		fmt.Println("Enter number of tickets:")
 		fmt.Scan(&userTickets)
 
-		remainingTickets = remainingTickets - userTickets
-		// append is similar to python, but not a method on the list. it's a function.
-		bookings = append(bookings, firstName+" "+lastName)
-		// fmt.Printf("The whole slice: %v\n", bookings)
-		// fmt.Printf("The first value: %v\n", bookings[0])
-		// fmt.Printf("Slice type: %T\n", bookings)
-		// fmt.Printf("Slice length: %v\n", len(bookings))
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		// isValidCity := city == "Singapore" || city == "London"
+		// isInvalidCity := city != "Singapore" && city != "London"
 
-		fmt.Printf("Thank you %v %v for purchasing %v tickets. \nAn email was sent to %v \n", firstName, lastName, userTickets, email)
-		fmt.Println("There are now", remainingTickets, "left")
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			// append is similar to python, but not a method on the list. it's a function.
+			bookings = append(bookings, firstName+" "+lastName)
+			// fmt.Printf("The whole slice: %v\n", bookings)
+			// fmt.Printf("The first value: %v\n", bookings[0])
+			// fmt.Printf("Slice type: %T\n", bookings)
+			// fmt.Printf("Slice length: %v\n", len(bookings))
 
-		firstNames := []string{}
-		// range allows us to iterate over elements
-		// this is like for loop with enumerate() in Python, but the index is not used
-		// _ makes the unused index value not be highlighted because the intellisense knows
-		for _, booking := range bookings {
-			// this splits the string using white space as a separator
-			var names = strings.Fields(booking)
-			// and then we take the first name and append it
-			firstNames = append(firstNames, names[0])
+			fmt.Printf("Thank you %v %v for purchasing %v tickets. \nAn email was sent to %v \n", firstName, lastName, userTickets, email)
+			fmt.Println("There are now", remainingTickets, "left")
+
+			firstNames := []string{}
+			// range allows us to iterate over elements
+			// this is like for loop with enumerate() in Python, but the index is not used
+			// _ makes the unused index value not be highlighted because the intellisense knows
+			for _, booking := range bookings {
+				// this splits the string using white space as a separator
+				var names = strings.Fields(booking)
+				// and then we take the first name and append it
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Println("Bookings (first name only shown):", firstNames)
+
+			// var noTicketsRemaining bool = remainingTickets == 0
+			// noTicketsRemaining := remainingTickets == 0
+			// if noTicketsRemaining {
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is fully booked.")
+				break // break loop, like Python
+			}
+
+			// & can also be used to print out a memory location for the variable value
+			// fmt.Println(&userName)
+			// } else if userTickets == remainingTickets {
+			// 	// do something else
+		} else {
+			if !isValidName {
+				fmt.Println("Your first name or last name is too short.")
+			}
+			if !isValidEmail {
+				fmt.Println("Your email does not contain an @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Your ticket number is invalid")
+			}
+			// we use if - else, but break and continue work like Python in for loops
+			// fmt.Printf("There are only %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+			// fmt.Println("Invalid data input")
 		}
-		fmt.Println("Bookings (first name only shown):", firstNames)
-
-		// & can also be used to print out a memory location for the variable value
-		// fmt.Println(&userName)
 	}
 
 }
